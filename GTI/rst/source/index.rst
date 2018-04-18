0be51014877ec3dad40c01f99c565ea9e80a5474
Maximilian Springenberg <maxi@dynip-129-217-119-157.wifi.tu-dortmund.de>
2018-04-18 11:10:08 +0200

    new structure

---
+++ GTI/rst/source/index.rst
@@ -0,0 +1,24 @@
+.. GTI documentation master file, created by
+   sphinx-quickstart on Wed Apr 18 10:43:02 2018.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to GTI's documentation!
+===============================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+Table Of Contents
+=================
+
+* :ref:`notes.rst`
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- update_index.py
+++ update_index.py
@@ -5,7 +5,10 @@ import os.path as path
 def gen_page(li):
     return '''
 <header>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"/>
+    <link 
+        rel="stylesheet" 
+        href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+        />
 </header>
 <body>
     <div class="mx-3 my-3">
@@ -18,12 +21,21 @@ def gen_page(li):
 
 if __name__ == '__main__':
     dir = path.dirname(path.realpath(__file__))
-    modules = [m for m in os.listdir(dir) if path.isdir(m)]
-    index_html = ['index.html' in os.listdir(m) for m in modules]
+    modules = [m 
+        for m in os.listdir(dir) 
+        if path.isdir('{}/rst/build/html'.format(m))
+        ]
+    index_html = ['index.html' in os.listdir('{}/rst/build/html'.format(m)) 
+        for m in modules
+        ]
     li_str = []
     for i, m in enumerate(modules):
         if index_html[i]:
-            li_str.append('<li><a href="{}/index.html">{}</a></li>'.format(m , m.split('/')[0]))
+            li_str.append(
+                '<li><a href="{}/rst/build/html/index.html">{}</a></li>'.format(
+                    m, m.split('/')[0]
+                    )
+                )
     index = gen_page(li_str)
     with open('index.html', 'w') as out:
         out.write(index)
