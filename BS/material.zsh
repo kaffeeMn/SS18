79541f93c45e287c53d4e8c03809574cbb796e4f
Maximilian Springenberg <maxi@dynip-129-217-071-029.wifi.tu-dortmund.de>
2018-04-26 09:23:57 +0200

    bs tut

--- BS/material.zsh
+++ BS/material.zsh
@@ -1,1 +1,2 @@
-open http://ess.cs.tu-dortmund.de/DE/Teaching/SS2018/BS/
+open http://ess.cs.tu-dortmund.de/DE/Teaching/SS2018/BS/ &
+open https://ess.cs.tu-dortmund.de/DE/Teaching/SS2018/BS/Downloads/
