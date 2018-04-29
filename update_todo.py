9d2a4aaa6c1100de55b9175be9b4303aeeed2aab
Maximilian Springenberg <maxi@mb-pro.local>
2018-04-29 11:02:58 +0200

    ...

--- update_todo.py
+++ update_todo.py
@@ -5,5 +5,9 @@ if __name__ == '__main__':
         f.close()
     lines_str = ''.join(lines)
     print(lines_str)
-    tasks = lines_str.split(':')[::2]
+    vals = lines_str.split(':')[2::2]
+    keys = lines_str.split(':')[1::2]
+    tasks = {}
+    for i in range(len(keys)):
+        tasks[keys[i]] = [v.rstrip() for v in vals[i].split('*')]
     print(tasks)
